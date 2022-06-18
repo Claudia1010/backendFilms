79be40027986031e2da348ed629ab784bf3cc3ce
claudia <claudiachristensen1@gmail.com>
2022-06-18 15:23:13 +0200

    database added

--- index.js
+++ index.js
@@ -10,9 +10,22 @@ const db = require('./db/db');
 
 const router = require('./router.js');
 
+const cors = require('cors');
+
+//Configuro cors
+
+let corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+};
+
 //Middlewares
 
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 app.use(router);
 
